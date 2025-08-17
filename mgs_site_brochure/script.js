51ae543f20ad73bbf03767f691b9a4ba04c2ddd9
christlymarian <christlymarian17@gmail.com>
2025-08-17 15:12:21 +0530

    Delete mgs_site_brochure directory

--- mgs_site_brochure/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  const toggle=document.querySelector('.menu-toggle');
-  const nav=document.getElementById('site-nav');
-  toggle.addEventListener('click',()=>{nav.classList.toggle('show')});
-});
